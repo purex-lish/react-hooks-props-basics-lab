e396ed80bca02516a96c902b608bc7a25faaac50
Purity Wambua <puritywamb@gmail.com>
2024-08-05 15:20:31 +0300

    Props

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,15 @@
 import React from "react";
+import user from "../data/user";
+import Links from "./Links";
 
-function About() {
+function About(user) {
+  
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {user.bio ? <p>{user.bio}</p> : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links />
     </div>
   );
 }
